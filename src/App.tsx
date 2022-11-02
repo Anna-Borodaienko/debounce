125b440cd918b94eb1dbb25f66582d9df19cc3b4
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-11-02 18:32:22 +0200

    Debounce solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useCallback, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function debounce(callback: (...args: any) => void, delay: number) {
+  let timerId: ReturnType<typeof setTimeout>;
+  
+  return function(...args: any) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(callback, delay, ...args);
+  }
 }
 
-export default App;
+export const App = () => {
+  const [value, setValue] = useState('');
+  const [appliedValue, setAppliedValue] = useState('');
+
+  const wrapper = useCallback(debounce(setValue, 1000), []);
+
+  return (
+    <>
+      <h2 className="title is-2">Debounce</h2>
+      <p></p>
+      <p className="subtitle is-4">value: {value}</p>
+      <input 
+        className="input is-rounded" 
+        type="text"
+        value={appliedValue}
+        onChange={event => {
+          setAppliedValue(event.target.value)
+          wrapper(event.target.value)
+        }
+        }
+      />
+    </>
+    
+  )
+}
